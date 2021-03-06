ee3a5bae90e836a09ca3c156d44c1ca74ec0c4a7
Maroo <manax@inbox.ru>
2021-03-06 13:55:08 +0300

    2. Модель данных. [#440692]

---
+++ src/main/java/ru/job4j/ru/job4j/pojo/College.java
@@ -0,0 +1,15 @@
+package ru.job4j.ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Vasiliy Petrov");
+        student.setGroup("MB1");
+        student.setAdmission(new Date());
+
+        System.out.println(student.getName() + " admitted to the group "
+                + student.getGroup() + " in a " + student.getAdmission());
+    }
+}
