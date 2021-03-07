1d11930104519a7b4f6dfd7eda1d0e961db5d6b6
Maroo <manax@inbox.ru>
2021-03-07 22:48:41 +0300

    3. Массивы и модели. [#440693]

---
+++ src/main/java/ru/job4j/ru/job4j/pojo/Library.java
@@ -0,0 +1,35 @@
+package ru.job4j.ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book b1 = new Book("Clean code", 464);
+        Book b2 = new Book("Idiot", 321);
+        Book b3 = new Book("Winnie the Pooh", 115);
+        Book b4 = new Book("Encyclopedia", 577);
+        Book[] books = new Book[4];
+        books[0] = b1;
+        books[1] = b2;
+        books[2] = b3;
+        books[3] = b4;
+        for (int i = 0; i < books.length; i++) {
+            Book bk = books[i];
+            System.out.println(bk.getName() + ", " + bk.getCount());
+        }
+        System.out.println("Replace \"Clean code\" to \"Encyclopedia\":");
+        Book tmp;
+        tmp = b1;
+        books[0] = b4;
+        books[3] = tmp;
+        for (int i = 0; i < books.length; i++) {
+            Book bk = books[i];
+            System.out.println(bk.getName() + ", " + bk.getCount());
+        }
+        System.out.println("Shown only \"Clean code\":");
+        for (int i = 0; i < books.length; i++) {
+            Book bk = books[i];
+            if (bk.getName() == "Clean code") {
+                System.out.println(bk.getName() + ", " + bk.getCount());
+            }
+        }
+    }
+}
